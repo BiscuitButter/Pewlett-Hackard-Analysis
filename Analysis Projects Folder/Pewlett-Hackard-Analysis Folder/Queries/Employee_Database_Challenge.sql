966933d71e160aa9ebf9dbec475a4da4d7c9297c
BiscuitButter <adamwomer@gmail.com>
2020-11-03 10:51:41 -0500

    deliverable 2

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
@@ -27,3 +27,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	ti.from_date,
+	ti.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN titles AS ti
+	ON ti.emp_no = e.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (ti.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
